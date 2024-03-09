81c0aa7f09d7ea9f30b29caecc4feb05abbcf388
atifraza158 <officialatifraza@gmail.com>
2024-03-09 14:11:13 +0500

    created desktop app for converting audio into video file using images

---
+++ audio_to_video.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+# from tkinter import ttk
+import ttkbootstrap as ttk
+import customtkinter as ctk
+
+# Main Window
+window = ctk.CTk()
+window.title("Audio to Video")
+window.iconbitmap("images/favicon/favicon.ico")
+window.geometry('700x500')
+window.minsize(700, 500)
+
+
+window.mainloop()
